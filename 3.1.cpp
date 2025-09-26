9c4d5062e4a5fe2cf11f339aa487c81370f99aa1
Ihor Bodak <igorbod689@gmail.com>
2025-09-26 08:48:03 +0300

    first commit

---
+++ 3.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double x, A, B, y;
+    cout << " x: ";
+    cin >> x;
+
+    A = 2; 
+
+    
+    if (x < 0) {
+        B = 2;
+    }
+    else if (x >= 0 && x < 1) {
+        B = 3;
+    }
+    else {
+        B = 4;
+    }
+
+    y = A + B;
+    cout << " y = " << y << endl;
+
+    return 0;
+}
+
