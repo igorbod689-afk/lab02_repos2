c90d389f5116d617f11860c44c7d822b7d0394f4
Ihor Bodak <igorbod689@gmail.com>
2025-10-02 16:40:34 +0300

    first commit

---
+++ 3.2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c, x, F;
+
+    cout << " a, b, c, x: ";
+    cin >> a >> b >> c >> x;
+
+    F = (x < 0 && b != 0) ? (a * x * x + b) :
+        ((x > 0 && b == 0) ? ((x - a) / (x - c)) : (x / c));
+
+    cout << "F = " << F << endl;
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c, x, F;
+
+    cout << "Введіть a, b, c, x: ";
+    cin >> a >> b >> c >> x;
+
+    if (x < 0 && b != 0) {
+        F = a * x * x + b;
+    }
+    else if (x > 0 && b == 0) {
+        F = (x - a) / (x - c);
+    }
+    else {
+        F = x / c;
+    }
+
+    cout << "F = " << F << endl;
+
+    return 0;
+}
